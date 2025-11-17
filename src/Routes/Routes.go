c7a27a0e82826cbae52cbd57a4d2cdc0589958a9
tjens23 <tjens23@student.sdu.dk>
2025-11-17 13:36:19 +0100

    fix: correct route path for adding a member to a group

--- src/Routes/Routes.go
+++ src/Routes/Routes.go
@@ -26,7 +26,7 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/groups", middleware.IsAuth, controllers.CreateGroup)
 	app.Patch("/groups/update/:id", middleware.IsAuth, controllers.UpdateGroup)
 	app.Delete("/groups/delete/:id", middleware.IsAuth, controllers.DeleteGroup)
-	app.Post("/groups/:id/add-member", middleware.IsAuth, controllers.AddMemberToGroup)
+	app.Post("/groups/:id/addmember", middleware.IsAuth, controllers.AddMemberToGroup)
 	app.Post("/groups/:id/remove-member", middleware.IsAuth, controllers.RemoveMemberFromGroup)
 
 	// Expense routes
